0928c23d57af1ecc7dd9f7ec08a5628083271cf7
MartinHarvey <martin.harvey.2017@uni.strath.ac.uk>
2018-03-14 15:48:16 +0000

    Fixed some of the .md file formatting.
    Minor changes to __main__.py

--- README.md
+++ README.md
@@ -1,19 +1,36 @@
-## Musical-Manipulator
+# Musical-Manipulator #
 Project using pyDub to manipulate and edit audio files.
 
 <a href = "https://github.com/MartinHarvey/Musical-Manipulator/blob/master/Changelog.md"> Changelog </a>
 
 
-#To Do:
+To Do:
+------
 <li>
-  Add Compatability for more file formats
+  Add Compatibility for more file formats
 <li>
   Add more manipulation functions
 <li>
   Add more GUI Elements
 
-#This project uses:
+This project uses:
+------------------
 
-The excellent <a href = https://github.com/jiaaro/pydub> pyDub </a> by jiaaro forms the core of this program. It handles all of the heavy lifting in a very easy to use, high-level syntax. It is easy to install and the source code is availible on GitHub.
+The excellent <a href = https://github.com/jiaaro/pydub> pyDub </a> by jiaaro forms the core of this program. It handles all of the heavy lifting in a
+very easy to use, high-level syntax. It is easy to install and the source code is available on GitHub.
 
-The UI elements of this project are provided by Tkinter, a set of modules that (AFAIK) are available out of the box with Python, and exists as the de-facto standard for creating interfaces in Python. It allows you to create professional and easy to understand GUIs, which are far better than controlling a program via the command line.
+The GUI elements of this project are provided by Tkinter, a set of modules that are available out of the box with Python, and exists as the de-facto
+standard for creating interfaces in Python. It allows you to create professional and easy to understand GUIs, which are far better than controlling a
+program via the command line.
+
+About:
+------
+This program was originally designed to help improve my python programming. Although still very elementary, it also helped me with
+using Git for version control. An external library, pyDub is used for all sound operations. It is very high level and is similar to
+regular Python. It has various functions for reversing, repeating audio, to name a few. I hope to implement more functions in the
+future.
+
+The file dialog ability comes from Tkinter, which allows GUI functions to be implemented in Python. It may be replaced by PyQt
+as I create a proper GUI, rather than having a CLI. I would prefer to use TKinter, as I believe it could be perfect for such a purpose
+(and as it the application uses the TKinter filedialog widget already), however, TKinter has proved difficult to use and the quality of
+online tutorials is low.
--- __main__.py
+++ __main__.py
@@ -2,7 +2,9 @@
 #using Git for version control. An external library, pyDub is used for all sound operations. It is very high level and is similar to
 #regular Python. It has various functions for reversing, repeating audio, to name a few. I hope to implement more functions in the
 #future. The file dialog ability comes from Tkinter, which allows GUI functions to be implemented in Python. It may be replaced by PyQt
-#as I create a proper GUI, rather than having a CLI.
+#as I create a proper GUI, rather than having a CLI. I would prefer to use TKinter, as I believe it could be perfect for such a purpose
+#(and as it the application uses the TKinter filedialog widget already), however, TKinter has proved difficult to use and the quality of
+#online tutorials is low.
 
 #Importing pyDub library
 from pydub import AudioSegment
@@ -19,13 +21,13 @@ loop = True
 #Getting File Path to Audio from user
 file_path = askopenfilename(initialdir = "/home/", title = "What file do you want to import?", filetypes = (("mp3 Files", "*.mp3"), ("all files", "*.*" )))
 
-#Importing audio file into variable trackusing Got
+#Importing audio file into variable track using AudioSegment function
 track = AudioSegment.from_mp3(file_path)
 
 #Loop continues until exit command is given
-while loop == True:
+while loop:
     #Users given choice of commands
-    print "You can reverse, duration, repeat, edit volume, merge tracks, check volume and exit"
+    print "You can reverse, check-length, repeat, edit volume, merge tracks, check volume and exit"
     #User command choice put into userChoice variable
     userChoice = raw_input("Please type in your choice. ")
 
@@ -35,7 +37,7 @@ while loop == True:
         track = track.reverse()
         print "Track Reversed."
 
-    if userChoice == "duration":
+    if userChoice == "check-length":
         #The duration_seconds function in pyDub returns the track length as a decimal value. This is better reoresented to the user as an integer in my personal opinion.
         #As result, the int function is then called to round the output of the duration_seconds seconds function to an easier to digest integer.
         print "The track is " + str(int(track.duration_seconds)) + " seconds long."
@@ -55,11 +57,11 @@ while loop == True:
         gain = raw_input("By how many dB do you wish to edit volume? ")
         #pyDub allows you to change volume using simple syntax outlined below
         track = track + gain
-        print "Gain changed by " + gain "."
+        print "Gain changed by " + gain + "."
 
     if userChoice == "merge tracks":
-        # This feature allows the user to concantenate a track onto the end of what they originally imported into the program
-        #THe following line calls in a fileDialog widget from Tkinter, allowing the user to select a file from any part of the users directory
+        #This feature allows the user to concantenate a track onto the end of what they originally imported into the program
+        #The following line calls in a fileDialog widget from Tkinter, allowing the user to select a file from any part of the users directory
         #The path of the imported file is stored in the variable 'file_path'
         file_path = askopenfilename(initialdir = "/home/", title = "What do you want to merge track with?", filetypes = (("mp3 Files", "*.mp3"), ("all files", "*.*" )))
         #The file is imported into pyDub using the AudioSegment function, allowing it to be edited using PyDub functions
